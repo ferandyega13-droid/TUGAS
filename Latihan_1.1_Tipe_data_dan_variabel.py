913a73bb09882d10ff447869fbcf4a6272b77bc7
ferandyega13-droid <ferandyega12@gmail.com>
2026-03-16 18:26:24 +0700

    Add files via upload

---
+++ Latihan_1.1_Tipe_data_dan_variabel.py
@@ -0,0 +1,14 @@
+nama = "laptop"
+panjang = 359
+berat = 2380
+use = True
+
+print ("saya punya",nama)
+print ("panjangnya",panjang,"mm")
+print("beratnya",berat,"gram")
+print("apakah laptop ini kamu gunakan?",use)
+
+print (type(nama))
+print (type(panjang))
+print (type(berat))
+print (type(use))
---
+++ Latihan_1.2_Input_type_data_dan_variabel.py
@@ -0,0 +1,9 @@
+nama = str(input("Masukkan nama barang "))
+panjang = float(input("Masukkan panjangnya "))
+berat = int(input("Masukkan beratnya "))
+use = bool(input("Apakah ini milikmu?"))
+
+print (type(nama))
+print (type(panjang))
+print (type(berat))
+print (type(use))
