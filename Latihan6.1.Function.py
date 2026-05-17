2f2cff04bc60efb84c9a61c9f68e981c68295f42
ferandyega13-droid <ferandyega12@gmail.com>
2026-05-17 18:54:21 +0700

    Create Latihan6.1.Function.py

---
+++ Latihan6.1.Function.py
@@ -0,0 +1,11 @@
+# Nama File: Latihan6.1.Function.py
+# Deskripsi: Program menggunakan fungsi tanpa parameter dan tanpa return.
+
+# Mendefinisikan fungsi bernama 'sapa_user'
+def sapa_user():
+    # Fungsi ini langsung mencetak kalimat ke layar saat dipanggil
+    print("Halo! Selamat datang di program latihan fungsi Python.")
+    print("Fungsi ini berjalan tanpa menerima parameter maupun mengembalikan nilai.")
+
+# Memanggil fungsi yang telah dibuat
+sapa_user()
